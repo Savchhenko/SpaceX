7a6d1d582d1f142347693114f37f4a65d3590e99
Alexandra Savchenko <svchhh@yandex.ru>
2022-09-24 17:25:32 +0300

    fix styles and parallax effect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// крутящийся шар:
 var instance = new vidbg(".video", {
 	mp4: "video/world.mp4", // URL or relative path to MP4 video
 	webm: "video/world.webm", // URL or relative path to webm video
@@ -7,38 +8,11 @@ var instance = new vidbg(".video", {
 	overlayAlpha: 0.3, // The overlay alpha. Think of this as the last integer in RGBA()
 });
 
-var rellax = new Rellax(".rocket");
+let rellax = new Rellax(".rocket", {
+    breakpoints:[576, 768, 1201]});
 
 if (document.body.clientWidth < 576) {
 	rellax.destroy();
 }
 
-AOS.init();
-
-$(document).ready(function () {
-	$(".anim").hover(
-		function () {
-			$(this).addClass("animated tada");
-		},
-		function () {
-			$(this).removeClass("animated tada");
-		}
-	);
-});
-
-// function animate(elem) {
-// 	var effect = elem.data("effect");
-// 	elem
-// 		.addClass("animated " + effect)
-// 		.one(
-// 			"webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-// 			function () {
-// 				elem.removeClass("animated " + effect);
-// 			}
-// 		);
-// }
-// $(document).ready(function () {
-// 	$(".anim").mouseenter(function () {
-// 		animate($(this));
-// 	});
-// });
+AOS.init(); // создаёт всплытие с небольшой задержкой футера и главного блока с видео при загрузке страницы
